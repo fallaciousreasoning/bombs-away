b9cde0987c758d3d94a26a3d7c2647a14ada3b9a
Jay Harris <jay.harris@outlook.co.nz>
2018-07-30 21:53:34 +1000

    Adds basic project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode: 'development',
+    entry: "./src/engine.ts",
+    output: {
+        filename: "main.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".ts", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.ts$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+};
