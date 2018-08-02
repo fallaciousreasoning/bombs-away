c50a7b3e439e8db9dd2a1107caca3b651525f746
Jay Harris <jay.harris@outlook.co.nz>
2018-08-02 23:11:25 +1000

    Adds a subject, to power systems

---
+++ src/iterators/subject.ts
@@ -0,0 +1,37 @@
+export class Subject<T> {
+    nextHandlers: ((item: T) => void)[];
+
+    map<K>(transform: (item: T) => K) {
+        const newSubject = new Subject<K>();
+        this.nextHandlers.push(item => newSubject.next(transform(item)));
+    }
+
+    filter(predicate: (item: T) => boolean) {
+        const newSubject = new Subject<T>();
+        this.nextHandlers.push(item => {
+            if (!predicate(item)) {
+                return;
+            }
+
+            newSubject.next(item);
+        });
+    }
+
+    reduce<To>(reducer: (prev: To, next: T) => To, initial: To) {
+        const newSubject = new Subject<To>();
+        let prev = initial;
+
+        this.nextHandlers.push(item => {
+            prev = reducer(prev, item);
+            newSubject.next(prev);
+        });
+    }
+
+    next(item: T) {
+        this.nextHandlers.forEach(handler => handler(item));
+    }
+
+    subscribe(subscription: (item: T) => void) {
+        this.nextHandlers.push(subscription);
+    }
+}
