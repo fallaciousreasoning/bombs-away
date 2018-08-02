d6d572cc5d5f9341cb9fd729f418ccf7e398a5bf
Jay Harris <jay.harris@outlook.co.nz>
2018-08-02 22:12:14 +1000

    implements a naive obserable list

---
+++ src/core/observableList.ts
@@ -0,0 +1,49 @@
+type ItemListener<T> = (item: T, index?: number) => void;
+
+export class ObservableList<T> {
+    items: T[]; 
+    addListeners: ItemListener<T>[] = [];
+    removeListeners: ItemListener<T>[] = [];
+
+    on(event: 'add' | 'remove', listener: ItemListener<T>) {
+        if (event === 'add') {
+            this.addListeners.push(listener);
+        }
+        if (event === 'remove') {
+            this.removeListeners.push(listener);
+        }
+    }
+
+    removeListener(listener: ItemListener<T>) {
+        let index = this.addListeners.indexOf(listener);
+        if (index !== -1) {
+            this.addListeners.splice(index, 0);
+            return;
+        }
+
+        index = this.removeListeners.indexOf(listener);
+        if (index !== -1) {
+            this.removeListeners.splice(index, 1);
+            return;
+        }
+    }
+
+    constructor(items?: T[]) {
+        this.items = items || [];
+    }
+
+    push(...items: T[]) {
+        this.items.push(...items);
+        items.forEach((item, index) => this.addListeners.forEach(listener => listener(item, index)));
+    }
+
+    pop(item: T) {
+        const index = this.items.indexOf(item);
+        if (index === -1) {
+            return;
+        }
+
+        this.items.splice(index, 1);
+        this.removeListeners.forEach(listener => listener(item, index));
+    }
+}
