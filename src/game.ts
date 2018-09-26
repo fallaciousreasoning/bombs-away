9c8d566afa89ed81a989347323a0cd518c5e9eb5
Jay Harris <jay.harris@outlook.co.nz>
2018-09-26 19:12:07 +1000

    Removes frame rate limit

--- src/game.ts
+++ src/game.ts
@@ -3,16 +3,13 @@ import { Engine } from "./engine";
 export const engine = new Engine();
 
 const tickEvent = { type: 'tick', step: 0 };
-const tickRate = 10;
 let lastTick = 0;
 
 const tick = (timestamp) => {
     const step = timestamp - lastTick;
-    if (step > tickRate) {
-        tickEvent.step = step;
-        engine.broadcastMessage(tickEvent);
-        lastTick = timestamp;
-    }
+    tickEvent.step = step;
+    engine.broadcastMessage(tickEvent);
+    lastTick = timestamp;
     requestAnimationFrame(tick);
 };
 
