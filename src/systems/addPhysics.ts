3b461a93b5e362b2ccb3416cad8a64f2c6136adb
Jay Harris <jay.harris@outlook.co.nz>
2018-09-26 21:35:23 +1000

    Updates physics to loop over bodies

--- src/systems/addPhysics.ts
+++ src/systems/addPhysics.ts
@@ -1,14 +1,14 @@
-import Body from '../components/body';
-import { Transform } from '../components/transform';
 import { Engine } from "../engine";
 
 export default function addPhysics(engine: Engine) {
     engine.subscriber
     .on('tick')
-    .with('body', 'transform')
-    .map(e => e.components)
-    .subscribe(({ body, transform }: { body: Body, transform: Transform }) => {
-        // TODO: Refactor this when I refactor systems
-        transform.position = transform.position.add(body.velocity.mul(1/60));
-    })
+    .allWith('body', 'transform')
+    .subscribe(({ entities, message }) => {
+        const step = message['step'];
+        entities.map(e => e.components)
+            .forEach(e1 => {
+                e1.transform.position = e1.transform.position.add(e1.body.velocity.mul(step));
+            })
+    });
 }
