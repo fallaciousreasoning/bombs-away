696bd81ddbb0c6ab2a7314e5408bc290a9010710
Jay Harris <jay.harris@outlook.co.nz>
2019-07-03 20:46:39 +1000

    Improves test

--- src/geometry/textureConverter.test.ts
+++ src/geometry/textureConverter.test.ts
@@ -11,7 +11,12 @@ test('A square is parsed correctly', () => {
 
     const conveter = new TextureConverter(grid);
     const result = conveter.getVertices();
-
-    console.log(result.map(r => r.length));
     expect(result.length).toBe(1);
+    expect(result[0].length).toBe(16);
+
+    const bounds = result[0].bounds;
+    expect(bounds.min.x).toBe(0);
+    expect(bounds.min.y).toBe(0);
+    expect(bounds.max.x).toBe(4);
+    expect(bounds.max.y).toBe(4);
 });
