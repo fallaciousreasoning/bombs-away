4f2ddb3f4b87e847be536a05a2a9e9715784facc
Jay Harris <jay.harris@outlook.co.nz>
2019-07-03 20:53:53 +1000

    Begins work on a test for parsing multiple polygons

--- src/geometry/textureConverter.test.ts
+++ src/geometry/textureConverter.test.ts
@@ -9,8 +9,8 @@ test('A square is parsed correctly', () => {
         [1,1,1,1,1],
     ];
 
-    const conveter = new TextureConverter(grid);
-    const result = conveter.getVertices();
+    const converter = new TextureConverter(grid);
+    const result = converter.getVertices();
     expect(result.length).toBe(1);
     expect(result[0].length).toBe(16);
 
@@ -19,4 +19,20 @@ test('A square is parsed correctly', () => {
     expect(bounds.min.y).toBe(0);
     expect(bounds.max.x).toBe(4);
     expect(bounds.max.y).toBe(4);
+});
+
+test('Multiple shapes can be parsed correctly', () => {
+    const grid = [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [1,1,0,0,0],
+        [1,1,1,1,1],
+        [1,1,1,1,1],
+    ];
+
+    const converter = new TextureConverter(grid);
+    const result = converter.getVertices();
+    console.log(result[0].length)
+    expect(result.length).toBe(2);
+
 });
