fe7023b9e9fe180d4b34d7820859040efbe0fdeb
Jay Harris <jay.harris@outlook.co.nz>
2020-01-21 16:16:06 +1100

    Changes min step

--- src/game.ts
+++ src/game.ts
@@ -12,7 +12,7 @@ const tickEvent: Tick = { type: 'tick', step: 0 };
 let lastTick = 0;
 
 const tick = (timestamp) => {
-    const step = Math.min(timestamp - lastTick, 250);
+    const step = Math.min(timestamp - lastTick, 60);
     tickEvent.step = 1/60;//step/1000;
     engine.broadcastMessage(tickEvent);
     lastTick = timestamp;
