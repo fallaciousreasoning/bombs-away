b97d5456737ea569f124d8371dbec44d2b60ffb4
Jay Harris <jay.harris@outlook.co.nz>
2020-01-22 18:52:42 +1100

    Starts work on touch control

--- src/core/input.ts
+++ src/core/input.ts
@@ -38,7 +38,7 @@ export default class Input {
             negativeKeys: [],
         },
         shoot: {
-            positiveKeys: [69]
+            positiveKeys: [69, 1]
         }
     };
 
@@ -47,9 +47,10 @@ export default class Input {
 
         document.addEventListener("keydown", event => this.setKey(event.which, true));
         document.addEventListener("keyup", event => this.setKey(event.which, false));
-        document.addEventListener("pointermove", event => this.setMousePos(event));
-        document.addEventListener("pointerdown", event => this.setKey(event.which, true));
-        document.addEventListener("pointerup", event => this.setKey(event.which, false));
+        document.addEventListener("mousemove", event => this.setMousePos(event));
+        document.addEventListener("touchmove", event => this.setMousePos({ x: event.touches[0].screenX, y: event.touches[0].screenY }));
+        document.addEventListener("mousedown", event => this.setKey(event.which, true));
+        document.addEventListener("mouseup", event => this.setKey(event.which, false));
     }
 
     getAxis(name: string): number {
@@ -69,10 +70,7 @@ export default class Input {
         this.downKeys[code] = down;
     }
 
-    setMousePos(event: PointerEvent) {
-        if (!event.isPrimary)
-            return;
-
+    setMousePos(event: { x: number, y: number }) {
         this.mousePosition = transformScreenToCanvas(new Vector2(event.x, event.y), this.on)
             .mul(METRES_A_PIXEL)
         // .add(getCamera().transform.position);
--- src/systems/playerController.ts
+++ src/systems/playerController.ts
@@ -4,16 +4,20 @@ import Input from "../core/input";
 import Vector2 from "../core/vector2";
 import { Engine } from "../engine";
 import { input } from "../game";
+import { getWidth } from "./addRenderer";
 
 export default function addPlayerController(engine: Engine) {
     engine
-    .makeSystem('player', 'body', 'transform')
-    .onEach('tick', ({ player, body, transform }, message) => {
-        const horizontal = input.getAxis('horizontal');
-        let jumpImpulse = 0;
-        if (input.getAxis('jump')) {
-            jumpImpulse = -player.jumpImpulse;
-        }
-        body.velocity = new Vector2(horizontal * 10 * message.step + body.velocity.x, jumpImpulse || body.velocity.y);
-    });
+        .makeSystem('player', 'body', 'transform')
+        .onEach('tick', ({ player, body, transform }, message) => {
+            let horizontal = input.getAxis('horizontal');
+            if (input.getAxis('shoot'))
+                horizontal += input.mousePosition.x < getWidth() / 2 ? -1 : 1;
+
+            let jumpImpulse = 0;
+            if (input.getAxis('jump')) {
+                jumpImpulse = -player.jumpImpulse;
+            }
+            body.velocity = new Vector2(horizontal * 10 * message.step + body.velocity.x, jumpImpulse || body.velocity.y);
+        });
 }
