81099a395442ae74b5480a11dd99acd0d3a578ed
Jay Harris <jay.harris@outlook.co.nz>
2020-01-22 11:16:47 +1100

    Increases explosion radius

--- src/components/explodes.ts
+++ src/components/explodes.ts
@@ -2,7 +2,7 @@ import { Entity } from "../entity";
 
 export default class Explodes {
     type: "explodes" = "explodes";
-    radius: number = 1;
+    radius: number = 1.5;
     with: (from: Entity) => Entity;
 
     constructor(w: (from: Entity) => Entity) {
