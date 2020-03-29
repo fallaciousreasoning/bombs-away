2e4a474b4cef9b82be61bae3440ef98bcd3452cd
Jay Harris <jay.harris@outlook.co.nz>
2020-03-29 13:22:53 +1100

    Adds a line for the current score

--- src/systems/addScoreTracker.ts
+++ src/systems/addScoreTracker.ts
@@ -1,20 +1,21 @@
 import Vector2 from '../core/vector2';
 import { Engine } from "../engine";
 import { canvas, context } from '../game';
+import { useGameView, getWidth } from './addRenderer';
 
 export default function addScoreTracker(engine: Engine) {
     // Render the current score.
     engine
         .makeSystem('score', 'transform')
         .onEach('tick', ({ score, transform }) => {
-            score.score = Math.max(score.score, Math.round(transform.position.y));
+            score.score = Math.max(score.score, transform.position.y);
             context['resetTransform']();
 
             context.font = "30px Arial";
             context.fillStyle = "black";
             context.textBaseline = 'top';
 
-            const text = `Score: ${score.score}`;
+            const text = `Score: ${Math.round(score.score)}`;
 
             context.fillText(text, 10, 10);
         });
@@ -22,6 +23,12 @@ export default function addScoreTracker(engine: Engine) {
     // Render a line for the current score.
     engine.makeSystem('score')
         .onEach('tick', ({ score }) => {
+            useGameView();
 
+            const stripeHeight = 0.2;
+            const stripeOffset = 0.8;
+
+            context.fillStyle = 'red';
+            context.fillRect(0, score.score + stripeHeight/2 + stripeOffset, getWidth(), stripeHeight);
         });
 }
