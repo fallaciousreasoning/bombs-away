e5564a839c31cf457598122d475d3253e3d1bad0
Jay Harris <jay.harris@outlook.co.nz>
2020-06-17 17:28:35 +1000

    Adds service worker

--- dist/sw.js
+++ dist/sw.js
@@ -16,7 +16,14 @@ const getResponse = async (event) => {
     // Try and fetch the resource from the network.
     // If that fails, serve what we have in the cache.
     try {
-        return await fetch(event.request);
+        const response = await fetch(event.request);
+
+        // Store the new response in the cache, so we don't serve
+        // stale data.
+        const cache = await caches.open(cacheName);
+        await cache.put(event.request, response.clone());
+
+        return  response;
     } catch (err) {
         const response = await caches.match(event.request);
         return response;
