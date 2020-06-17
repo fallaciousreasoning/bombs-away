6dd8877dbad36e660e081362e267484dd4bacf9a
Jay Harris <jay.harris@outlook.co.nz>
2020-06-17 16:06:15 +1000

    no power up on load

--- src/components/powerupable.ts
+++ src/components/powerupable.ts
@@ -3,5 +3,5 @@ import Powerup from "./powerup";
 
 export default class Powerupable {
     type: 'powerupable' = 'powerupable';
-    powerups: Powerup['power'][] = ['invulnerable'];
+    powerups: Powerup['power'][] = [];
 }
